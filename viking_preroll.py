4db03f7eae26b17f3ac3bc20d04f7e791358f3be
Ben Romney <bromney3@byu.net>
2017-11-04 12:07:25 -0600

    Operational viking preroll script

--- viking_preroll.py
+++ viking_preroll.py
@@ -1,5 +1,5 @@
-#Authors: Daniel Fuller, Brennan Mitchell, Trevor Barrus, Ben Romney
-### contact this guy: Ben Romney
+# Authors: Brennan Mitchell, Sierra Davis, Ben Romney
+# contact this guy: Ben Romney
 # run this one BEFORE the sim script
 
 import os
@@ -16,7 +16,7 @@ STARTPRE = -25
 
 
 ############################
-## PRE-ROLL VIKING ANIMATION ##
+# PRE-ROLL VIKING ANIMATION
 # this version has each rig control name with the prefix "viking_rig_main_" 
 # because the layout scenes have that (the rig file doesn't)
 ############################
@@ -82,8 +82,6 @@ def clearScale(list):
 
 
 #Selects/Returns Full Rig
-#TODO: What about accessories? do we need to clear transformations on those too?
-# update: it looks like all the accessories + beard go with the transformation, so that's cool 
 def selectRig():
     print ">>SelectRig() starting"
 
@@ -155,44 +153,29 @@ def selectRig():
     'viking_rig_main_Viking_RGT_FK_upper_arm_cc_01']    
 
     viking_hands = [
+    #Note: Viking has no ring finger
     'viking_rig_main_Viking_LFT_hand_cupping_splaying_cc_01',     #Left
     'viking_rig_main_Viking_LFT_thumb_primary_cc_01',
-    'viking_rig_main_Viking_LFT_thumb_MED_secondary_cc_01',
     'viking_rig_main_Viking_LFT_thumb_DIS_secondary_cc_01',
     'viking_rig_main_Viking_LFT_index_finger_primary_cc_01',
     'viking_rig_main_Viking_LFT_index_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_index_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_LFT_index_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_LFT_middle_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_middle_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_LFT_middle_finger_primary_cc_01',
     'viking_rig_main_Viking_LFT_middle_metacarpal_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_ring_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_ring_finger_MED_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_ring_finger_primary_cc_01',
-    'viking_rig_main_Viking_LFT_ring_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_LFT_pinky_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_pinky_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_LFT_pinky_finger_primary_cc_01',
     'viking_rig_main_Viking_LFT_pinky_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_hand_cupping_splaying_cc_01',	#Right
     'viking_rig_main_Viking_RGT_thumb_primary_cc_01',
-    'viking_rig_main_Viking_RGT_thumb_MED_secondary_cc_01',
     'viking_rig_main_Viking_RGT_thumb_DIS_secondary_cc_01',
     'viking_rig_main_Viking_RGT_index_finger_primary_cc_01',
     'viking_rig_main_Viking_RGT_index_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_index_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_RGT_index_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_middle_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_middle_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_RGT_middle_finger_primary_cc_01',
     'viking_rig_main_Viking_RGT_middle_metacarpal_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_ring_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_ring_finger_MED_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_ring_finger_primary_cc_01',
-    'viking_rig_main_Viking_RGT_ring_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_pinky_finger_DIS_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_pinky_finger_MED_secondary_cc_01',
     'viking_rig_main_Viking_RGT_pinky_finger_primary_cc_01',
     'viking_rig_main_Viking_RGT_pinky_metacarpal_secondary_cc_01']
 
@@ -228,7 +211,7 @@ def selectRig():
     fullRig = viking_main + viking_head + viking_mouth + viking_neck + viking_torso + viking_arms + viking_hands + viking_hips + viking_legs_feet
     
     #Create Selection from 'fullRig'
-    mc.select(fullRig, replace=True)
+    mc.select(viking_hands, replace=True)
     return fullRig
 
 
@@ -240,24 +223,22 @@ def keyArmFK():
     if (mc.getAttr('viking_rig_main_Viking_LFT_arm_settings_cc_01.FK_IK', keyable=True) or mc.getAttr('viking_rig_main_Viking_LFT_arm_settings_cc_01.FK_IK', channelBox=True)):
         mc.setKeyframe('viking_rig_main_Viking_LFT_arm_settings_cc_01.FK_IK');
         
-
-   # mc.setAttr('ten_rig_main_r_arm_switch_CTL.IKFK_Switch', 1) 
+    #mc.setAttr('ten_rig_main_r_arm_switch_CTL.IKFK_Switch', 1) 
     mc.setAttr('viking_rig_main_Viking_RGT_arm_settings_cc_01.FK_IK', 0)  #really not sure about this 
 
     if (mc.getAttr('viking_rig_main_Viking_RGT_arm_settings_cc_01.FK_IK', keyable=True) or mc.getAttr('viking_rig_main_Viking_RGT_arm_settings_cc_01.FK_IK', channelBox=True)):
         mc.setKeyframe('viking_rig_main_Viking_RGT_arm_settings_cc_01.FK_IK');
 
 def fingerNames():
+    #Note: Viking has no ring finger
     return [
     'viking_rig_main_Viking_LFT_thumb_primary_cc_01',
     'viking_rig_main_Viking_LFT_index_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_LFT_middle_metacarpal_secondary_cc_01',
-    'viking_rig_main_Viking_LFT_ring_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_LFT_pinky_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_thumb_primary_cc_01',
     'viking_rig_main_Viking_RGT_index_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_middle_metacarpal_secondary_cc_01',
-    'viking_rig_main_Viking_RGT_ring_metacarpal_secondary_cc_01',
     'viking_rig_main_Viking_RGT_pinky_metacarpal_secondary_cc_01'
 ]
     
@@ -275,12 +256,8 @@ def keyFingers():
 
 def APose():
     print ">>APose() starting"
-    #Handle Right Arm
-    #   mc.rotate(0, 0, -45, 'ten_rig_main_r_armRoot_FK_CTL')
-    mc.rotate(0, 0, -5, 'viking_rig_main_Viking_LFT_FK_upper_arm_cc_01') #45 degrees seems to be too straight for A-pose...
-    #Handle Left Arm
-    #   mc.rotate(0, 0, -45, 'ten_rig_main_l_armRoot_FK_CTL')
-    mc.rotate(0, 0, -5, 'viking_rig_main_Viking_RGT_FK_upper_arm_cc_01') 
+    mc.rotate(0, 0, -45, 'viking_rig_main_Viking_LFT_FK_upper_arm_cc_01')
+    mc.rotate(0, 0, -45, 'viking_rig_main_Viking_RGT_FK_upper_arm_cc_01') 
 
 
 def setRigKey(fullRig):
@@ -316,37 +293,21 @@ clearRotate(fullRig)
 clearTranslate(fullRig)
 clearScale(fullRig)
 
-
 #scaleFingers() #Scale Fingers (Only Scalable Control)
  #might not need this since we have clearScale()
 
-#Key APose (Adjust Arms, Keyframe)
-APose()
+#APose() -- No need to call APose() because Viking was built in A-Pose
 setRigKey(fullRig)
-
-
 
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='translateX')
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='translateY')
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='translateZ')
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='translateX') #Hip Controller Not Working?  <-- existing comment
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='translateY')
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='translateZ')
 
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='rotateX')
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='rotateY')
 mc.setKeyframe('viking_rig_main_Viking_COG_cc_01', at='rotateZ')
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='rotateX')
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='rotateY')
-#mc.setKeyframe('ten_rig_main_m_COG_CTL', at='rotateZ')
 
-# I'm not sure why these are in here, just the right shoulder?
-#mc.setKeyframe('ten_rig_main_r_shoulder_CTL', at='translateX') #Shoulder Controller Not Working?   <-- existing comment
-#mc.setKeyframe('ten_rig_main_r_shoulder_CTL', at='translateY')
-#mc.setKeyframe('ten_rig_main_r_shoulder_CTL', at='translateZ')
-
-#Export Alembic (Requires User Input - Select Ten's Rig)
+#Export Alembic (Requires User Input - Select Viking's Rig)
 mc.playbackOptions(animationStartTime=STARTPRE)
 import alembic_exporter
 alembic_exporter.go()
-
